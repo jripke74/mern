15998a55ef1025713e7af351f3f96022dde9cb6b
Jeff Ripke <jripke74@gmail.com>
2024-02-08 10:22:23 -0800

    add two way binding

--- frontend/src/components/NewGoal/NewGoal.jsx
+++ frontend/src/components/NewGoal/NewGoal.jsx
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import './NewGoal.css';
 
 export default function NewGoal({ onAddGoal }) {
+  const [enteredText, setEnteredText] = useState('');
+
   const addGoalHandler = function (event) {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new goal',
+      text: enteredText,
     };
+
+    setEnteredText('');
 
     onAddGoal(newGoal);
   };
 
+  const textChangeHandler = (event) => {
+    setEnteredText(event.target.value);
+  };
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );

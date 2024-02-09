eb99ad7d1f94e3336483efff95ea4061834a7a10
Jeff Ripke <jripke74@gmail.com>
2024-02-08 16:19:23 -0800

    add Routes, NewPlace and Users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Routes,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import './App.css';
 
 import Goals from './goals/components/Goals/Goals.jsx';
+import Users from './user/pages/Users.jsx';
+import NewPlace from './places/pages/NewPlace.jsx';
 
 function App() {
   return (
     <Routes>
-      <Route path="/goals" exact>
-        <Goals />
-      </Route>
+      <Switch>
+        <Route path="/goals" exact>
+          <Goals />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </Routes>
   );
 }
---
+++ frontend/src/places/pages/NewPlace.jsx
@@ -0,0 +1,3 @@
+export default function NewPlace() {
+  return <h2>NewPlace Works!</h2>;
+}

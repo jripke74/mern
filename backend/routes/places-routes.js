7c64847cc0d2d68513a5ff2cbb02d841e65337e5
Jeff Ripke <jripke74@gmail.com>
2024-02-17 15:15:41 -0800

    add pId

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({ message: 'It works!' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: '20 W 34th St, New Your, NY 10001',
+    createrId: 'u1',
+  },
+];
+
+router.get('/:pId', (req, res, next) => {
+  const placeId = req.params.pId;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;

626290c078517b6ca56cc76151c906f063f7dd6f
Jeff Ripke <jripke74@gmail.com>
2024-02-23 11:53:23 -0800

    add createdPlace

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -3,6 +3,7 @@ const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
+const Place = require('../models/place');
 
 let DUMMY_PLACES = [
   {
@@ -63,18 +64,23 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  const createdPlace = {
-    id: uuid(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
-    creator: creator,
-  };
+    location: coordinates,
+    image: 'https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg',
+    creator,
+  });
 
-  DUMMY_PLACES.push(createdPlace);
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError('Creating place failed, please try again', 500);
+    return next(error);
+  }
 
-  res.status(200).json({ place: createdPlace });
+  res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = (req, res, next) => {

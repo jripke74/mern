c28ac44a2f286979dba85d2e2a86faa9f8178baa
Jeff Ripke <jripke74@gmail.com>
2024-02-26 10:54:04 -0800

    add login logic

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -63,21 +63,26 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-  if (hasUser) {
-    return next(
-      new HttpError('Could not create user, eamil already exists.', 422)
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try agian later.',
+      500
     );
+    return next(error);
   }
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    return next(
-      new HttpError('Could not identy user, credentials seem to be wrong.', 401)
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
     );
+    return next(error);
   }
 
   res.json({ mesage: 'Logged in' });

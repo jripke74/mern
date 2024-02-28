9890ac75d50044f71c86991373ef88b0536c9815
Jeff Ripke <jripke74@gmail.com>
2024-02-28 11:36:51 -0800

    add login logic

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -58,10 +58,34 @@ export default function Auth() {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true);
+
     if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5003/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again.');
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch('http://localhost:5003/api/users/signup', {
           method: 'POST',
           headers: {
@@ -82,7 +106,6 @@ export default function Auth() {
         setIsLoading(false);
         auth.login();
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || 'Something went wrong, please try again.');
       }

61572b186107a07395255f17bd66d570380e1c3a
Jeff Ripke <jripke74@gmail.com>
2024-03-14 11:00:39 -0700

    add proces.env.PORT

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mxuwfw1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5003);
+    app.listen(process.env.PORT || 5003);
   })
   .catch((err) => {
     console.log(err);

9c383f324529d781e0a6d421fbe82dbe6e0ed48a
Jeff Ripke <jripke74@gmail.com>
2024-03-21 09:21:13 -0700

    add res.writeP

--- backend/app2.js
+++ backend/app2.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<h1>Hello</h1>');
+    return res.end();
+  }
 });
 
 server.listen(3000);

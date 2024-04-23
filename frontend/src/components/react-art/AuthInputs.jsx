6ed0454d0455a659b8b23a2e508c85906523499a
Jeff Ripke <jripke74@gmail.com>
2024-04-23 06:58:44 -0700

    misc

--- frontend/src/components/react-art/AuthInputs.jsx
+++ frontend/src/components/react-art/AuthInputs.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './AuthInputs.css';
+import classes from './AuthInputs.module.css';
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -22,13 +22,19 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id={classes['auth-inputs']}>
+      <div className={classes.controls}>
         <p>
-          <label>Email</label>
+          <label
+            className={`${classes.label} ${
+              emailNotValid ? classes.invalid : ''
+            }`}
+          >
+            Email
+          </label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            className={emailNotValid ? classes.invalid : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
@@ -36,18 +42,18 @@ export default function AuthInputs() {
           <label>Password</label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className={passwordNotValid ? classes.invalid : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className={classes.actions}>
+        <button type="button" className={classes['text-button']}>
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <button className={classes.button} onClick={handleLogin}>
           Sign In
         </button>
       </div>

f7596009033a041a2b18cbae775ef0997d65a92e
Jeff Ripke <jripke74@gmail.com>
2024-04-30 10:18:50 -0700

    add data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

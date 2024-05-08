e3154cbf8682b1a51f5e9d3487f438eaf05d9a38
Jeff Ripke <jripke74@gmail.com>
2024-05-08 08:44:08 -0700

    add timer and logic

--- frontend/src/components/final-countdown/TimerChallenge.jsx
+++ frontend/src/components/final-countdown/TimerChallenge.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+
 import classes from './TimerChallenge.module.css';
 
 export default function TimerChalleng({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
   return (
     <section className={classes.challenge}>
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className={classes.challengeTime}>
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={classes.timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
     </section>
   );
 }

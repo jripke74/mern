e1ae40c29a565062db5e56c3128c2fbcf6d62012
Jeff Ripke <jripke74@gmail.com>
2024-05-16 11:55:09 -0700

    misc adds

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -171,7 +171,7 @@ function App() {
           <Users />
         </Route>
         <Route path="/project-tracker">
-          <main className="h-screen my-8">
+          <main className="h-screen my-8 flex gap-8">
             <ProjectSidebar />
             <NewProject />
           </main>

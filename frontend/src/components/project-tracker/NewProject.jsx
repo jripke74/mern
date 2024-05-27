759a1d40045f02ace389a3068b226a49b17df365
Jeff Ripke <jripke74@gmail.com>
2024-05-27 12:14:41 -0700

    adjust styles

--- frontend/src/components/project-tracker/NewProject.jsx
+++ frontend/src/components/project-tracker/NewProject.jsx
@@ -33,11 +33,11 @@ export default function NewProject({ onAdd, onCancel }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Okay">
-        <h2 className="text-xl fond-bold text-stone-700 my-4">Invalid Input</h2>
-        <p className="text-stone-600 mb-4">
+        <h2 className="text-xl fond-bold text-stone-900 my-4">Invalid Input</h2>
+        <p className="text-stone-800 mb-4">
           Oops ... looks like you forgot to enter a value.
         </p>
-        <p className="text-stone-600 mb-4">
+        <p className="text-stone-800 mb-4">
           Please make sure you provide a valid value for every input field.
         </p>
       </Modal>

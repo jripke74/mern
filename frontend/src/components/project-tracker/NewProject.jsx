e51c05b0f55c95a76203d0b0cb449d210384cd7c
Jeff Ripke <jripke74@gmail.com>
2024-05-27 09:34:45 -0700

    add useRef()

--- frontend/src/components/project-tracker/NewProject.jsx
+++ frontend/src/components/project-tracker/NewProject.jsx
@@ -1,6 +1,12 @@
+import { useRef } from 'react';
+
 import Input from './Input.jsx';
 
 export default function NewProject() {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">

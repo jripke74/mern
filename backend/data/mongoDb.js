d9f7ae6fd64a556226ab19df95baec8b42ff2728
Jeff Ripke <jripke74@gmail.com>
2024-06-06 11:30:19 -0700

    add data

--- backend/data/mongoDb.js
+++ backend/data/mongoDb.js
@@ -686,3 +686,9 @@ db.users.deleteMany({}); // deletes everything in that collection
 db.users.drop(); // deletes entire collections
 
 db.dropDatabase(); // deletes db
+
+db.contacts.explain().find({ 'dob.age': { $gt: 60 } });
+
+db.contacts.explain('executionStats').find({ 'dob.age': { $gt: 60 } });
+
+db.contacts.createIndex({ 'dob.age': 1 });

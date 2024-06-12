cd116e5011553a15db23b1a8376360808bcdbb41
Jeff Ripke <jripke74@gmail.com>
2024-06-12 14:30:13 -0700

    add ProgressBar.jsx

--- frontend/src/components/place-picker/DeleteConfirmation.jsx
+++ frontend/src/components/place-picker/DeleteConfirmation.jsx
@@ -1,22 +1,11 @@
 import { useEffect, useState } from 'react';
 
 import classes from './DeleteCofirmation.module.css';
+import ProgressBar from './ProgressBar';
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRemainingTime((prevTime) => prevTime - 10);
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
@@ -39,7 +28,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }

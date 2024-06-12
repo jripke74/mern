29613b95096ec87f17d1b1a626a2254a4e0bca2c
Jeff Ripke <jripke74@gmail.com>
2024-06-12 09:22:09 -0700

    add open

--- frontend/src/components/place-picker/PlacePickerModal.jsx
+++ frontend/src/components/place-picker/PlacePickerModal.jsx
@@ -1,28 +1,17 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 import classes from './PlacePickerModal.module.css';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
-
   return createPortal(
-    <dialog className={classes.modal} ref={dialog}>
+    <dialog className={classes.modal} ref={dialog} open={open}>
       {children}
     </dialog>,
     document.getElementById('modal')
   );
-});
+}
 
 export default Modal;

56d62c11ae46a96a07107c94a6ee6f35a9e82138
Jeff Ripke <jripke74@gmail.com>
2024-06-14 08:10:42 -0700

    add data query

--- backend/data/mongoDb.js
+++ backend/data/mongoDb.js
@@ -821,3 +821,15 @@ db.places.find({
 });
 
 db.places.deleteOne({ _id: ObjectId('666b6435d0f4d66dba54eb55') });
+
+const p1 = [-122.4547, 37.77473];
+const p2 = [-122.45303, 37.76641];
+const p3 = [-122.51026, 37.76411];
+const p4 = [-122.51088, 37.77131];
+db.places.find({
+  location: {
+    $geoWithin: {
+      $geometry: { type: 'Polygon', coordinates: [[p1, p2, p3, p4, p1]] },
+    },
+  },
+});

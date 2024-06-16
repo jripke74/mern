17479d0cfed8623a7fee08076c8d6440e63439f1
Jeff Ripke <jripke74@gmail.com>
2024-06-16 08:37:28 -0700

    add questions and answer

--- frontend/src/components/quiz-app/Quiz.jsx
+++ frontend/src/components/quiz-app/Quiz.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
+import QUESTIONS from './assets/questions.js';
 import classes from './QuizHeader.module.css';
 
 export default function Quiz() {
-  return <p>Currently active Question</p>;
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  const activeQuestionIndex = userAnswer.length;
+
+  return (
+    <div id={classes.quiz}>
+      <div id={classes.question}>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id={classes.answers}>
+          <li key={answer} className={classes.answer}>
+            <button>{answer}</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }

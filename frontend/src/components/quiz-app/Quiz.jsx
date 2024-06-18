d7b7216f0d8f27823637cb91a415f687d7c23aa9
Jeff Ripke <jripke74@gmail.com>
2024-06-18 10:05:17 -0700

    add quisIsComplete

--- frontend/src/components/quiz-app/Quiz.jsx
+++ frontend/src/components/quiz-app/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import QUESTIONS from './assets/questions.js';
+import quizCompleteImg from './assets/quiz-complete.png';
 import classes from './Quiz.module.css';
 
 export default function Quiz() {
@@ -8,18 +9,32 @@ export default function Quiz() {
 
   const activeQuestionIndex = userAnswer.length;
 
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswer((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div className={classes.summary}>
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shufffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shufffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id={classes.quiz}>
       <div id={classes.question}>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id={classes.answers}>
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shufffledAnswers.map((answer) => (
             <li key={answer} className={classes.answer}>
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}

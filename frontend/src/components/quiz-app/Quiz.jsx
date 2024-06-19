57a1c5bca1e867985a4824b85de52cb7647ca80d
Jeff Ripke <jripke74@gmail.com>
2024-06-19 07:26:08 -0700

    add highlight selected answers

--- frontend/src/components/quiz-app/Quiz.jsx
+++ frontend/src/components/quiz-app/Quiz.jsx
@@ -6,19 +6,34 @@ import quizCompleteImg from './assets/quiz-complete.png';
 import classes from './Quiz.module.css';
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState('');
   const [userAnswers, setUserAnswer] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === '' ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
-    setUserAnswer((prevUserAnswers) => {
-      return [...prevUserAnswers, selectedAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setAnswerState('answered');
+      setUserAnswer((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState('correct');
+        } else {
+          setAnswerState('wrong');
+        }
+
+        setTimeout(() => {
+          setAnswerState('');
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null), [handleSelectAnswer];
@@ -46,13 +61,32 @@ export default function Quiz() {
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id={classes.answers}>
-          {shufffledAnswers.map((answer) => (
-            <li key={answer} className={classes.answer}>
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+          {shufffledAnswers.map((answer) => {
+            const isSeleceted = userAnswers[userAnswers.length - 1] === answer;
+            let cssClass = '';
+
+            if (answerState === 'answered' && isSeleceted) {
+              cssClass = classes.selected;
+            }
+
+            if (
+              (answerState === 'correct' || answerState === 'wrong') &&
+              isSeleceted
+            ) {
+              cssClass = answerState;
+            }
+
+            return (
+              <li key={answer} className={classes.answer}>
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>

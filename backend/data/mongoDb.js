9759fd51661c65b13964f350d43d43c0fe30a01a
Jeff Ripke <jripke74@gmail.com>
2024-06-24 13:55:24 -0700

    add queries

--- backend/data/mongoDb.js
+++ backend/data/mongoDb.js
@@ -917,6 +917,48 @@ db.persons.aggregate([
 
 db.persons.aggregate([
   { $match: { gender: 'female' } },
-  { $group: { _id: { state: '$location.state' }, totalPerson: { $sum: 1 } } },
+  { $group: { _id: { state: '$location.state' }, totalPersons: { $sum: 1 } } },
   { $sort: { totalPersons: -1 } },
+]);
+
+db.persons.aggregate([
+  { $match: { 'dob.age': { $gt: 50 } } },
+  {
+    $group: {
+      _id: { gender: '$gender' },
+      numPerson: { $sum: 1 },
+      avgAge: { $avg: '$dob.age' },
+    },
+  },
+  { $sort: { numPerson: -1 } },
+]);
+
+db.persons.aggregate([
+  {
+    $project: {
+      _id: 0,
+      gender: 1,
+      fullName: {
+        $concat: [
+          { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+          {
+            $substrCP: [
+              '$name.first',
+              1,
+              { $subtract: [{ $strLenCP: '$name.first' }, 1] },
+            ],
+          },
+          ' ',
+          { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+          {
+            $substrCP: [
+              '$name.last',
+              1,
+              { $subtract: [{ $strLenCP: '$name.last' }, 1] },
+            ],
+          },
+        ],
+      },
+    },
+  },
 ]);

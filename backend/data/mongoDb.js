7cb210368c32587269d866ba69087e69e735b163
Jeff Ripke <jripke74@gmail.com>
2024-06-26 09:27:05 -0700

    add queries

--- backend/data/mongoDb.js
+++ backend/data/mongoDb.js
@@ -1093,3 +1093,29 @@ db.friends.aggregate([
   },
   { $sort: { maxScore: -1 } },
 ]);
+
+db.persons.aggregate([
+  {
+    $bucket: {
+      groupBy: '$dob.age',
+      boundaries: [18, 30, 40, 50, 60, 70, 120],
+      output: {
+        numPersons: { $sum: 1 },
+        averageAge: { $avg: '$dob.age' },
+      },
+    },
+  },
+]);
+
+db.persons.aggregate([
+  {
+    $bucketAuto: {
+      groupBy: '$dob.age',
+      buckets: 5,
+      output: {
+        numPersons: { $sum: 1 },
+        averageAge: { $avg: '$dob.age' },
+      },
+    },
+  },
+]);

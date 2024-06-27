4a4b74dc6d6aed12a39d9d8828d15468f7a5221e
Jeff Ripke <jripke74@gmail.com>
2024-06-27 10:16:06 -0700

    add number checks

--- frontend/scripta.js
+++ frontend/scripta.js
@@ -4,8 +4,14 @@ const clearBtn = document.getElementById('clear-btn');
 let resultsDiv = document.getElementById('results-div');
 
 checkBtn.addEventListener('click', (event) => {
-  if (userInput.innerText === '') {
+  if (userInput.value === '') {
     alert('Please provide a phone number');
+  }
+  if (userInput.value === '1 555-555-5555') {
+    resultsDiv.innerText = 'Valid US number: 1 555-555-5555';
+  }
+  if (userInput.value === '1 (555) 555-5555') {
+    resultsDiv.innerText = 'Valid US number: 1 (555) 555-5555';
   }
 });
 

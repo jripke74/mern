ed90f8ddca5237ddf0dc320a715571e9666a02d8
Jeff Ripke <jripke74@gmail.com>
2024-06-30 13:12:09 -0700

    add styles

--- frontend/src/components/counter-app/CounterHeader.jsx
+++ frontend/src/components/counter-app/CounterHeader.jsx
@@ -1,11 +1,12 @@
 import { log } from './log.js';
 import logoImg from './assets/logo.png';
+import classes from './CounterHeader.module.css';
 
 export default function Header() {
   log('<Header /> rendered', 1);
 
   return (
-    <header id="main-header">
+    <header id={classes['main-header']}>
       <img src={logoImg} alt="Magnifying glass analyzing a document" />
       <h1>React - Behind The Scenes</h1>
     </header>

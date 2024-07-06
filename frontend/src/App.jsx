2fd19e84bb7c3a8021ba42e4b9e12b236ad23238
Jeff Ripke <jripke74@gmail.com>
2024-07-06 08:27:36 -0700

    add counter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -382,6 +382,7 @@ function App() {
             <main>
               <ConfigureCounter onSet={handleSetCount} />
               <Counter initialCount={chosenCount} />
+              <Counter initialCount={0} />
             </main>
           </>
         </Route>

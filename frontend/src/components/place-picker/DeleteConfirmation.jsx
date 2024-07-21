c7691bca77991b339c3a8d020976148d53ddd81d
Jeff Ripke <jripke74@gmail.com>
2024-07-21 09:42:12 -0700

    add text

--- frontend/src/components/place-picker/DeleteConfirmation.jsx
+++ frontend/src/components/place-picker/DeleteConfirmation.jsx
@@ -19,7 +19,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
   return (
     <div id={classes['delete-confirmation']}>
       <h2>Are you really sure?</h2>
-      <p>Do you really want to remove this place?</p>
+      <p>Do you want to remove this place?</p>
       <div id={classes['confirmation-actions']}>
         <button onClick={onCancel} className={classes['button-text']}>
           No
